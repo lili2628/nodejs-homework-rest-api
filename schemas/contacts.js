e8cfbe442e35fe3337e4923cfa24418d48c5c5c0
Liliia Paliichuk <113315152+lili2628@users.noreply.github.com>
2023-07-09 14:38:10 +0300

    finish

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -4,9 +4,15 @@ const addSchema = Joi.object({
   name: Joi.string().min(2).max(255).required(),
   email: Joi.string().min(4).max(255).required().email(),
   phone: Joi.string().required().pattern(/^([+]?[0-9\s-\(\)]{3,25})*$/i),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 
 module.exports = {
     addSchema,
+    updateFavoriteSchema,
 }
